04a932cdde208d97056ca08eecd1332daa9e9a17
Zak Tonks <zak.tonks@enable.com>
2024-04-26 09:34:04 +0100

    Save Foos

--- Program.cs
+++ Program.cs
@@ -8,10 +8,32 @@ optionsBuilder.UseCosmos(
     "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
     "repro-context");
 
-var reproContext = new ReproDbContext(
+var reproContext1 = new ReproDbContext(
     optionsBuilder.Options);
 
-var query = reproContext.Foos.ToQueryString();
+reproContext1.Foos.Add(
+    new Foo()
+    {
+        Bar = "A",
+        Baz = "B",
+    });
+
+reproContext1.Foos.Add(
+    new Foo()
+    {
+        Bar = "C",
+        Baz = "D",
+    });
+
+reproContext1.SaveChanges();
+
+var reproContext2 = new ReproDbContext(
+    optionsBuilder.Options);
+
+var query = reproContext2.Foos.ToQueryString();
 
 Console.WriteLine(query); // No filter - select all from root
 
+var foos = reproContext2.Foos.ToList();
+
+Console.WriteLine(foos.Count);
